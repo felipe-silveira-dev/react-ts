09b849c496f35c25af5a3dfbdf880f10affc88bb
felipe-silveira-dev <felipe.silveira4@fatec.sp.gov.br>
2023-11-01 12:59:05 -0300

    useEffect

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import './Home.css'
 import homeImage from '../../assets/971.jpg'
+import { useState, useEffect } from "react"
 
 interface myProps {
     title: string,
@@ -7,11 +8,25 @@ interface myProps {
 }
 
 const Home = (props : myProps) => {
+  const [completed, setCompleted] = useState(false);
+  const [task, setTask] = useState('');
+
+  useEffect(() => {
+    if (completed) {
+        setTask('Congratulations on completing your task!');
+    }
+  }, [completed])
+
   return (
     <>
+        <h1>Todo List</h1>
+        <h3>{task}</h3>
+        <button onClick={() => setCompleted(true)}>Concluir tarefa!</button>
+
         <h1 className='title'>{props.title}</h1>
         <p className='description'>{props.description}</p>
         <img src={homeImage} alt="home" className='img' />
+
     </>
   )
 }
